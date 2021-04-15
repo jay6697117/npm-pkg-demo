6b830e6ef9c4408fa6a4f7c52c2759f086baa043
steve zhang <871506263@qq.com>
2021-04-16 00:01:18 +0800

    first commit

---
+++ demo.js
@@ -0,0 +1,31 @@
+// 1.0 lodash
+// import { cloneDeep } from 'lodash';
+// console.log('cloneDeep :>> ', cloneDeep);
+// const obj = { a: 1, b: 2 };
+// const newObj = cloneDeep(obj);
+// console.log('obj :>> ', obj);
+// newObj.a = 111;
+// newObj.b = 222;
+// console.log('newObj :>> ', newObj);
+
+// 2.0 qs
+// import { parse, stringify } from 'qs';
+// console.log('parse :>> ', parse);
+// console.log('stringify :>> ', stringify);
+// const obj = parse('name=zjh&age=33&sex=man&height=177');
+// console.log('obj :>> ', obj);
+// const str = stringify({ name: 'zjh', age: 33, sex: 'man', height: '177' });
+// console.log('str :>> ', str);
+
+// 3.0 numeral
+// import numeral from 'numeral';
+// console.log('numeral :>> ', numeral);
+// // 格式化
+// const res = numeral(10000.23).format("0,0.0"); // '10,000'
+// console.log('res :>> ', res); // res :>>  10,000.2
+
+// 4.0 dayjs
+import dayjs from 'dayjs';
+console.log('dayjs :>> ', dayjs);
+const res = dayjs().format('YYYY/MM/DD HH:mm:ss');
+console.log('res :>> ', res);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+console.log('webpack :>> ', webpack);
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+console.log('process.env.NODE_ENV :>> ', process.env.NODE_ENV);
+console.log(path.join(__dirname, 'dist'));
+
+const webpackConfig = {
+  mode: process.env.NODE_ENV, // 模式配置
+  entry: {
+    demo: './demo.js'
+  }, // 入口文件
+  output: {
+    // 添加hash可以防止文件缓存，每次都会生成8位的hash串
+    filename: '[name].[hash:8].js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: ''
+  }, // 出口文件
+  // module: {}, // 处理对应模块
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 用哪个html作为模板在public目录下创建一个index.html页面当做模板来用
+      template: './index.html',
+      hash: true
+    })
+  ], // 对应的插件
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true
+  } // 开发服务器配置
+};
+
+console.log('webpackConfig :>> \n', webpackConfig);
+
+module.exports = webpackConfig;
